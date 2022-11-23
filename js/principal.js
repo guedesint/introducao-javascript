9417bad5386d339c7aa8267f6aca6bbb77c162b1
GuedesNOT <guedesint@gmail.com>
2022-11-22 22:15:16 -0300

    fazendo verificacao de peso

---
+++ js/principal.js
@@ -0,0 +1,27 @@
+let titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+
+var tdImc = paciente.querySelector(".info-imc");
+tdImc.textContent = imc;
+
+
+
+
+if (peso < 0 || peso > 1000) {
+  alert("peso invalido")
+}
+
+console.log(peso);
+console.log(altura);
+console.log(imc);
