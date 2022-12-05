895258da0a9670c61124bc92fb4aa8f84e80f198
guedesint <guedesint@gmail.com>
2022-12-05 18:37:09 -0300

    começando o filtro minuto 3

---
+++ js/remover-paciente.js
@@ -0,0 +1,11 @@
+var tabela = document.querySelector("#tabela-pacientes");
+
+
+tabela.addEventListener("dblclick", (event) => {
+
+  event.target.parentNode.classList.add("fadeOut");
+  
+  setTimeout(() => {
+    event.target.parentNode.remove();
+  }, 500);
+})
