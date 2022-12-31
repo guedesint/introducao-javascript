772e199057dd85be246e103c49216213e40dbe91
GuedesPC <guedesint@gmail.com>
2022-12-31 19:11:46 -0300

    removendo

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -4,11 +4,8 @@ var tabela = document.querySelector("#tabela-pacientes");
 tabela.addEventListener("dblclick", (event) => {
 
   event.target.parentNode.classList.add("fadeOut");
-  
+
   setTimeout(() => {
     event.target.parentNode.remove();
   }, 500);
 })
-
-
-console.log("testando git");
